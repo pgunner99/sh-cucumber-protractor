8137035f3164dd397b5efdd8ba376d8bf5976ea9
JDraugelis <jon08940@sh129957.local>
2015-01-11 04:15:46 -0500

    New support and step functions
    Including; Given I’m logged into Myhealth, Given I’m logged into
    priority quote, When I create small group in priority quote.

---
+++ features/homepagePQ.feature
@@ -0,0 +1,21 @@
+Feature: Homepage 
+  As a user
+  I want to visit the homepage
+  So that I can access the various features on offer
+
+    Scenario: Successful Login
+    	Given I am logged into Priority Quote with user "routphil" and password "test"
+    	#When I click on Small Group
+    	When I create a small group in Priority Quote
+    	#Given I am on Priority Quote homepage
+    	#When I enter text "routphil" in textbox with (name) "username"
+    	#When I enter text "test" in textbox with (name) "password"
+    	#When I click on Priority Quote Login
+    	#When I wait "10000" milliseconds
+		#When I enter text "test" in textbox with (ID) "recentSearch"
+		#When I wait "5000" milliseconds
+    	
+    	#When I click on the "messages" id
+    	#When I wait "1000" milliseconds
+    	#Then I should see a "UPCOMING EVENTS" link 	
+    	#Then I should see the text "MyHealth"
--- features/step_definitions/homepage/steps.js
+++ features/step_definitions/homepage/steps.js
@@ -2,47 +2,66 @@ var support = require('../support');
 
 var steps = function() {
 
-  this.Given(/^I am on the homepage$/, function(callback) {
+  this.Given(/^I am logged into MyHealth with user "([^"]*)" and password "([^"]*)"$/, function(user, password, callback) {
+      console.log("\n Log into MyHealth with user(" + user + ") and password(" + password + ")");
+      support.logIntoMyHealth(this, user, password, function(result){
+      setTimeout(callback, 1000);
+    });
+  });
+  
+  this.Given(/^I am logged into Priority Quote with user "([^"]*)" and password "([^"]*)"$/, function(user, password, callback) {
+      console.log("\n Log into Priority Quote with user(" + user + ") and password(" + password + ")");
+      support.logIntoPriorityQuote(this, user, password, function(result){
+      setTimeout(callback, 1000);
+    });
+  });
+
+  
+  this.Given(/^I am on MyHealth homepage$/, function(callback) {
+  console.log("\n Navigate to My Health homepage");
     support.get(this, 'https://wwwtest.internal.priorityhealth.com/app/universal-login-ui/#/login?appId=MyHealth-QA2', function(result){
       setTimeout(callback, 1000);
     });
   });
-
-  this.Then(/^I should see a "([^"]*)" link$/, function(link, callback) {
-    support.findByBinding(this, link, function(result){
-      result.getText().then (function(text){
-        text.trim().toLowerCase().should.equal(link.trim().toLowerCase());             
-        setTimeout(callback, 1000);
-      });     
+  
+  this.Given(/^I am on Priority Quote homepage$/, function(callback) {
+  console.log("\n Navigate to Priority Quote homepage");
+    support.getPQ(this, 'https://wwwtest.internal.priorityhealth.com/app/priority-quote-beta/#/login?returnUrl=%2Flogin', function(result){
+      setTimeout(callback, 1000);
     });
   });
-
-  this.Then(/^I should not see a "([^"]*)" link$/, function(link, callback) {
-    support.isElementPresent(this, link, function(result){
-      result.should.equal(false);
+  
+  this.When(/^I create a small group in Priority Quote$/, function(callback) {
+  console.log("\n Click on Small Group");
+      support.createSmallGroupInPriorityQuote(this, function(result){
       setTimeout(callback, 1000);
     });
   });
 
-  this.Then(/^I should see a "([^"]*)"$/, function(link, callback) {
-    support.isElementPresentByClass(this, link, function(result){
-      result.should.equal(true);
+  this.When(/^I enter text "([^"]*)" in textbox with \(ID\) "([^"]*)"$/, function(text, textbox, callback) {
+      console.log("\n Enter text with values, text(" + text + ") in the textbox with ID (" + textbox + ")");
+      support.enterTextInTextBoxId(this, text, textbox, function(result){
       setTimeout(callback, 1000);
     });
   });
-
-
-  this.When(/^I enter text "([^"]*)" in "([^"]*)"$/, function(text, textbox, callback) {
-      console.log("\n Enter in enter text step with values text(" + text + ") textbox(" + textbox + ")");
-
-      support.enterTextInTextBox(this, text, textbox, function(result){
+  
+  this.When(/^I enter text "([^"]*)" in textbox with \(name\) "([^"]*)"$/, function(text, textbox, callback) {
+      console.log("\n Enter text with values, text(" + text + ") in the textbox with name(" + textbox + ")");
+      support.enterTextInTextBoxName(this, text, textbox, function(result){
       setTimeout(callback, 1000);
     });
   });
   
   this.When(/^I click on MyHealth Login$/, function(callback) {
   console.log("\n Click on MyHealth Login");
-      support.clickOnMyhealthLogin(this, 'input[type=\"submit\"]', function(result){
+      support.clickOnCssLink(this, 'input[type=\"submit\"]', function(result){
+      setTimeout(callback, 1000);
+    });
+  });
+  
+  this.When(/^I click on Priority Quote Login$/, function(callback) {
+  console.log("\n Click on \"Priority Quote\" Login");
+      support.clickOnCssLink(this, 'input.ph-btn.btn-green', function(result){
       setTimeout(callback, 1000);
     });
   });
@@ -68,6 +87,30 @@ var steps = function() {
       	setTimeout(callback, 1000);
     });
   });
+
+/* Example functions
+  this.Then(/^I should see a "([^"]*)" link$/, function(link, callback) {
+    support.findByBinding(this, link, function(result){
+      result.getText().then (function(text){
+        text.trim().toLowerCase().should.equal(link.trim().toLowerCase());             
+        setTimeout(callback, 1000);
+      });     
+    });
+  });
+
+  this.Then(/^I should not see a "([^"]*)" link$/, function(link, callback) {
+    support.isElementPresent(this, link, function(result){
+      result.should.equal(false);
+      setTimeout(callback, 1000);
+    });
+  });
+
+  this.Then(/^I should see a "([^"]*)"$/, function(link, callback) {
+    support.isElementPresentByClass(this, link, function(result){
+      result.should.equal(true);
+      setTimeout(callback, 1000);
+    });
+  });*/
 
 
 };
